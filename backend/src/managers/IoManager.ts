4401f49d28e302d927d6d639f3684f43578e23a4
Harsh Tailor <harshtailor8696@gmail.com>
2025-05-22 19:32:09 +0530

    Frontend Initialized

--- backend/src/managers/IoManager.ts
+++ backend/src/managers/IoManager.ts
@@ -1,21 +1,18 @@
-import { Server } from "http";
-import http from 'http'
+import http from 'http';
 
+import { Server } from "socket.io";
 const server = http.createServer();
 
+
 export class IoManager {
+    private static io: Server;
 
-        private static io : Server;
-        private static instance : IoManager;
-
-        public static getIo()
-        {
-            if(!this.instance){
-
-                this.instance = new IoManager();
-                const io = new Server(server);
-                this.io = io;
-            }   
-            return this.io;
+    public static getIo() {
+        if (!this.io) {
+            const io = new Server(server);
+            this.io = io;
         }
+        return this.io;
+    }
+    
 }
